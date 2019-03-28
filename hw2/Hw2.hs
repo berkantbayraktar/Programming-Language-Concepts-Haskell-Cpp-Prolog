7e12c0adee783f5f4a022e633148e9a546811dbd
Berkant Bayraktar <berkantbayraktar06@gmail.com>
2019-03-28 12:28:18 +0300

    implement bind normal

--- hw2/Hw2.hs
+++ hw2/Hw2.hs
@@ -14,4 +14,24 @@ isNumber :: String -> Bool
 isNumber [] = True
 isNumber (x:xs) = (x >= '0' && x <= '9' || x== '-') && (isNumber xs)
 
+getDatum (ASTNode datum _ _ ) = datum
+getLeft (ASTNode _ left _ ) = left
+getRight (ASTNode _ _ right) = right
 
+
+bindNormal (ASTNode datum left right) = 
+    case datum of
+        ASTLetDatum var ->  case (getDatum right) of
+                                ASTSimpleDatum dat ->  ASTNode (ASTSimpleDatum dat) left left
+                                ASTLetDatum dat -> bindNormal right
+        _ -> ASTNode datum left right    
+
+
+eagerEvaluation (ASTNode datum left right) = 
+    case datum of 
+        ASTLetDatum "let" -> ASTError "err"
+        ASTSimpleDatum "num" -> ASTError "err" 
+
+
+--eagerEvaluation (ASTNode (ASTLetDatum "x") (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "3") EmptyAST EmptyAST) EmptyAST) (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "4") EmptyAST EmptyAST) EmptyAST)) (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST) (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST)))
+--eagerEvaluation (ASTNode (ASTLetDatum "x") ((ASTNode (ASTSimpleDatum "negate") (ASTNode (ASTSimpleDatum "times") (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "1") EmptyAST EmptyAST) EmptyAST) (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "2") EmptyAST EmptyAST) EmptyAST)) (ASTNode (ASTSimpleDatum "len") (ASTNode (ASTSimpleDatum "cat") (ASTNode (ASTSimpleDatum "str") (ASTNode (ASTSimpleDatum "CE") EmptyAST EmptyAST) EmptyAST) (ASTNode (ASTSimpleDatum "str") (ASTNode (ASTSimpleDatum "NG") EmptyAST EmptyAST) EmptyAST)) EmptyAST)) EmptyAST)) (ASTNode (ASTSimpleDatum "times") (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST) (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST) (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST))))
