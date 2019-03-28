e3558ef1158add65b5cffca22f6c2ec0a0155147
Berkant Bayraktar <berkantbayraktar06@gmail.com>
2019-03-28 13:16:39 +0300

    implement evaluate

--- hw2/Hw2.hs
+++ hw2/Hw2.hs
@@ -9,7 +9,7 @@ isNumber :: String -> Bool
 --normalEvaluation :: AST -> ASTResult
 -- DO NOT MODIFY OR DELETE THE LINES ABOVE -- 
 -- IMPLEMENT isNumber, eagerEvaluation and normalEvaluation FUNCTIONS ACCORDING TO GIVEN SIGNATURES -- 
-
+evaluate :: AST -> String
 
 isNumber [] = True
 isNumber (x:xs) = (x >= '0' && x <= '9' || x== '-') && (isNumber xs)
@@ -24,14 +24,27 @@ bindNormal (ASTNode datum left right) =
         ASTLetDatum var ->  case (getDatum right) of
                                 ASTSimpleDatum dat ->  ASTNode (ASTSimpleDatum dat) left left
                                 ASTLetDatum dat -> bindNormal right
-        _ -> ASTNode datum left right    
+        _ -> ASTNode datum left right
+        
+
+evaluate (ASTNode (ASTSimpleDatum "cat") left right) = evaluate left ++ evaluate right
+evaluate (ASTNode (ASTSimpleDatum "str") left right) = evaluate left
+evaluate (ASTNode (ASTSimpleDatum "len") left right) = show (length (evaluate left ))
+evaluate (ASTNode (ASTSimpleDatum "plus") left right) = show ((read (evaluate left) :: Integer) + (read (evaluate right) :: Integer ))
+evaluate (ASTNode (ASTSimpleDatum "times") left right) = show ((read (evaluate left) :: Integer) * (read (evaluate right) :: Integer ))
+evaluate (ASTNode (ASTSimpleDatum "negate") left right) = show (-(read (evaluate left) :: Integer))
+evaluate (ASTNode (ASTSimpleDatum "num") left right) = evaluate left
+evaluate (ASTNode (ASTSimpleDatum str) left right) = str
 
 
-eagerEvaluation (ASTNode datum left right) = 
-    case datum of 
-        ASTLetDatum "let" -> ASTError "err"
-        ASTSimpleDatum "num" -> ASTError "err" 
+
+normalEvaluation all@(ASTNode datum left right) = evaluate (bindNormal all)
+
+
+
+--eagerEvaluation (ASTNode datum left right) =
 
 
 --eagerEvaluation (ASTNode (ASTLetDatum "x") (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "3") EmptyAST EmptyAST) EmptyAST) (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "4") EmptyAST EmptyAST) EmptyAST)) (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST) (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST)))
 --eagerEvaluation (ASTNode (ASTLetDatum "x") ((ASTNode (ASTSimpleDatum "negate") (ASTNode (ASTSimpleDatum "times") (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "1") EmptyAST EmptyAST) EmptyAST) (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "2") EmptyAST EmptyAST) EmptyAST)) (ASTNode (ASTSimpleDatum "len") (ASTNode (ASTSimpleDatum "cat") (ASTNode (ASTSimpleDatum "str") (ASTNode (ASTSimpleDatum "CE") EmptyAST EmptyAST) EmptyAST) (ASTNode (ASTSimpleDatum "str") (ASTNode (ASTSimpleDatum "NG") EmptyAST EmptyAST) EmptyAST)) EmptyAST)) EmptyAST)) (ASTNode (ASTSimpleDatum "times") (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST) (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST) (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST))))
+--(ASTNode (ASTLetDatum "x") (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "2") EmptyAST EmptyAST) EmptyAST) (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "3") EmptyAST EmptyAST) EmptyAST)) (ASTNode (ASTLetDatum "x") (ASTNode (ASTSimpleDatum "num") (ASTNode (ASTSimpleDatum "4") EmptyAST EmptyAST) EmptyAST) (ASTNode (ASTSimpleDatum "plus") (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST) (ASTNode (ASTSimpleDatum "x") EmptyAST EmptyAST))))
