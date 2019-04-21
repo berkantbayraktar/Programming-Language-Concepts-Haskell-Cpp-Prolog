280db4788414baf78999f30b032b763597faa622
Berkant Bayraktar <berkantbayraktar06@gmail.com>
2019-04-21 15:00:24 +0300

    implement race.cpp

--- hw3/Homework 3/Race.cpp
+++ hw3/Homework 3/Race.cpp
@@ -75,7 +75,57 @@ int Race::getNumberOfCarsinRace(){
 }
 
 void Race::goBacktoLap(int lap){
-    
+
 }
 
+void Race::operator++(){
+    Car *temp = head;
+    while(temp != NULL){
+        temp->Lap(Utilizer::generateAverageLaptime());
+        temp = temp-> getNext();
+    }
+}
 
+void Race::operator--(){
+    Car *temp = head;
+    if(temp == NULL)
+        return;
+    else{
+        while(temp->getNext() != NULL){
+            temp = temp->getNext();
+        }
+        temp = NULL;
+    }
+}
+
+Car Race::operator[](const int car_in_position){
+    Car *temp = head;
+    for(int i = 0 ; i < car_in_position ; i++)
+    {
+        temp = temp->getNext();
+        if(temp == NULL)
+            break;
+    }
+    return *temp;
+
+}
+
+Car Race::operator[](std::string driver_name){
+    Car *temp = head;
+    while(temp != NULL){
+        if(temp->getDriverName() == driver_name)
+            return *temp;
+        temp = temp->getNext();    
+    }
+    return *temp;
+}
+
+Race& Race::operator=(const Race& rhs){
+    this->head = rhs.head;
+    this->race_name = rhs.race_name;
+    this->average_laptime = rhs.average_laptime;
+}
+
+std::ostream& operator<<(std::ostream& os, const Car& car){
+
+}
