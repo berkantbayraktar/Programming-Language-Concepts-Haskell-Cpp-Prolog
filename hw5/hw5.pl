a6387ca995addc5846851bb2c984430062a88de5
Berkant Bayraktar <berkantbayraktar06@gmail.com>
2019-05-17 23:39:21 +0300

    Update

--- hw5/hw5.pl
+++ hw5/hw5.pl
@@ -33,12 +33,22 @@ ionic_pair(A,B,Number):-
     catomic_number(A,CatomicA), catomic_number(B,CatomicB),
     Total is CatomicA + CatomicB, Total = Number.
 
-molecule(Catom_List,0).
+
 molecule(Catom_List, Total_Catomic_Number) :-
+    moleculess(Catom_List,Total_Charge,Total_Catomic_Number),
+    sum(Total_Charge,Summation),
+    Summation = 0.
+  
+
+moleculess(Catom_List,T,0) :-
+    T = [],
+    Catom_List = [].
+moleculess(Catom_List,T,Total_Catomic_Number) :-
     between(0,Total_Catomic_Number,N),
     N1 is Total_Catomic_Number - N,
     catomic_number(Neym,N1),
+    ion(Neym,C),
     Rest is Total_Catomic_Number - N1,
-    molecule(Rest_List,Rest),
+    moleculess(Rest_List,R_T,Rest),
+    T = [C| R_T],
     Catom_List = [Neym | Rest_List].
-    
