ebaa15d047a62dd1496b785b32945f6563c4ab4b
Berkant Bayraktar <berkantbayraktar06@gmail.com>
2019-05-19 02:54:01 +0300

    FINAL HW5

--- hw5/hw5.pl
+++ hw5/hw5.pl
@@ -10,17 +10,19 @@ ion( Name , Charge ):-
     last(L,Last),
     (Last =< 4 -> Charge is Last ; Charge is (Last - 8)).
 
-findAllPossible(Catom_List,T,0) :-
+findAllPossible(Catom_List,T,0,_) :- 
     T is 0 ,
     Catom_List = [].
 
-findAllPossible(Catom_List,Total_Charge,Total_Catomic_Number) :-
+findAllPossible(Catom_List,Total_Charge,Total_Catomic_Number,LL) :-
     Total_Catomic_Number > 0,
     catomic_number(Name,Current),
+    Current >=  LL,
     Current =< Total_Catomic_Number,
     ion(Name,Charge),    
     Rest is Total_Catomic_Number - Current,
-    findAllPossible(Rest_List,Remaining_Charge,Rest),
+    ML is Current,
+    findAllPossible(Rest_List,Remaining_Charge,Rest,ML),
     Total_Charge is Charge + Remaining_Charge,
     Catom_List = [Name | Rest_List].
 
@@ -32,6 +34,5 @@ eleminateDuplicates([H,M|Rest]) :-
     eleminateDuplicates([M|Rest]).
 
 molecule(Catom_List, Total_Catomic_Number) :-
-    findAllPossible(Catom_List,Total_Charge,Total_Catomic_Number),
-    Total_Charge = 0,
-    eleminateDuplicates(Catom_List).
+    findAllPossible(Catom_List,Total_Charge,Total_Catomic_Number,0),
+    Total_Charge = 0.
